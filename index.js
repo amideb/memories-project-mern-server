afb35e3a036f7dfe0d00eb8cb87423ef18c95e11
Debrup Mondal <debrup.official.365@gmail.com>
2021-03-04 20:56:55 +0530

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import dotenv from 'dotenv'
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+dotenv.config();
 
 
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
